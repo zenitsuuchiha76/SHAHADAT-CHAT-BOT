9f6ce6132220bce72c41938c78a2b9ccdfb9362c
SHAHADAT SAHU <sahushahadat@gmail.com>
2025-08-27 22:28:35 +0600

    Create fork.js

---
+++ Script/commands/fork.js
@@ -0,0 +1,19 @@
+module.exports.config = {
+name: "fork",
+version: "1.0.0",
+hasPermssion: 0,
+credits: "SHAHADAT SAHU",
+description: "Send GitHub repo link",
+commandCategory: "other",
+usages: "fork",
+cooldowns: 3,
+};
+
+module.exports.run = async function({ api, event }) {
+return api.sendMessage(
+"🔗 GitHub Repo Link:\n\nhttps://github.com/shahadat-sahu/SHAHADAT-CHAT-BOT.git",
+event.threadID,
+event.messageID
+);
+};
+
