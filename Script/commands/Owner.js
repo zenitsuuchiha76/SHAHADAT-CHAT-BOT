ed97ab27917b33250caafcdcf1f7e573aa45b0bb
SHAHADAT SAHU <sahushahadat@gmail.com>
2025-09-02 21:49:14 +0600

    Update Owner.js

--- Script/commands/Owner.js
+++ Script/commands/Owner.js
@@ -1,38 +1,64 @@
+const request = require("request");
+const fs = require("fs-extra");
+
 module.exports.config = {
- name: "owner",
- version: "1.0.1",
- hasPermssion: 0,
- credits: "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐈𝐬𝐥𝐚𝐦",
- description: "Owner information command with styled box",
- commandCategory: "Information",
- usages: "",
- cooldowns: 5
+  name: "owner",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "SHAHADAT SAHU",
+  description: "Show Owner Info with styled box & random photo",
+  commandCategory: "Information",
+  usages: "owner",
+  cooldowns: 2
 };
 
-module.exports.run = async ({ api, event }) => {
- const ownerInfo = 
-`╔═════════════════════╗
-║ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 
-╠═════════════════════╣
-║ 👤 𝗡𝗮𝗺𝗲 : 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓
+module.exports.run = async function ({ api, event }) {
+
+  
+  const info = `
+╔═════════════════════ ✿
+║ ✨ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ✨
+╠═════════════════════ ✿
+║ 👑 𝗡𝗮𝗺𝗲 : 𝗦𝗛𝗔𝗛𝗔𝗗𝗔𝗧 𝗦𝗔𝗛𝗨
 ║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗦𝗔𝗛𝗨
 ║ 🎂 𝗔𝗴𝗲 : 𝟭𝟴+
 ║ 💘 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗶𝗻𝗴𝗹𝗲
 ║ 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : 𝗦𝘁𝘂𝗱𝗲𝗻𝘁
 ║ 📚 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : 𝗛𝗦𝗖
 ║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗞𝗵𝗮𝗴𝗿𝗮𝗰𝗵𝗮𝗿𝗶
-╠═════════════════════╣
-║ 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦 
-╠═════════════════════╣
-║ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : 
-║ https://fb.com/100001039692046
-║ 💬 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿 : 
+╠═════════════════════ ✿
+║ 🔗 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
+╠═════════════════════ ✿
+║ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :
+║ fb.com/100001039692046
+║ 💬 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿 :
 ║ m.me/100001039692046
-║ 📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 : 
-║ https://wa.me/01882333052
-║ ✈️ 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 : 
-║ https://t.me/yoursahu
-╚═════════════════════╝`;
+║ 📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 :
+║ wa.me/01882333052
+║ ✈️ 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 :
+║ t.me/yoursahu
+╚═════════════════════ ✿
+`;
 
- return api.sendMessage(ownerInfo, event.threadID, event.messageID);
+  const images = [
+    "https://i.imgur.com/8WBso8x.png",
+    "https://i.imgur.com/0VZu5eY.png",
+    "https://i.imgur.com/bkixgPK.jpeg",
+    "https://i.imgur.com/z6G6L4c.jpeg"
+  ];
+
+  const randomImg = images[Math.floor(Math.random() * images.length)];
+
+  const callback = () => api.sendMessage(
+    {
+      body: info,
+      attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
+    },
+    event.threadID,
+    () => fs.unlinkSync(__dirname + "/cache/owner.jpg")
+  );
+
+  return request(encodeURI(randomImg))
+    .pipe(fs.createWriteStream(__dirname + "/cache/owner.jpg"))
+    .on("close", () => callback());
 };
