2d33aff80e19dc0b881a22f07b88351ae944e96f
zenitsuuchiha76 <zenitsuuchiha76@gmail.com>
2025-10-29 11:30:11 +0600

    Update gali_reply.js

--- Script/commands/gali_reply.js
+++ Script/commands/gali_reply.js
@@ -12,9 +12,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Shahadat Bokasoda")==0 || event.body.indexOf("Sahadat mc")==0 || event.body.indexOf("chod")==0 || event.body.indexOf("Sahadat nodir pola")==0 || event.body.indexOf("bc")==0 || event.body.indexOf("Shahadat re chudi")==0 || event.body.indexOf("shahadat re chod")==0 || event.body.indexOf("Shahadat Abal")==0 || event.body.indexOf("Shahadat Boakachoda")==0 || event.body.indexOf("Shahadat madarchod")==0 || event.body.indexOf("Sahadat re chudi")==0 || event.body.indexOf("Sahu Bokachoda")==0) {
+	if (event.body.indexOf("Gojo Bokasoda")==0 || event.body.indexOf("Gojo mc")==0 || event.body.indexOf("chod")==0 || event.body.indexOf("Gojo nodir pola")==0 || event.body.indexOf("bc")==0 || event.body.indexOf("Gojo re chudi")==0 || event.body.indexOf("Gojo re chod")==0 || event.body.indexOf("piw piw Abal")==0 || event.body.indexOf("Piw Piw Boakachoda")==0 || event.body.indexOf("Gojo madarchod")==0 || event.body.indexOf("Gojo re chudi")==0 || event.body.indexOf("Gojo Bokachoda")==0) {
 		var msg = {
-				body: "তোর মতো বোকাচোদা রে আমার বস শাহাদাৎ চু*দা বাদ দিছে🤣\nসাহু এখন আর hetars চুষে না🥱😈",
+				body: "তোর মতো বোকাচোদা রে আমার বস গোজু চু*দা বাদ দিছে🤣\nসাহু এখন আর hetars চুষে না🥱😈",
 			}
 			api.sendMessage(msg, threadID, messageID);
 		}
