49e8ea974b493f29eda5430b248328fe3ae0d8f0
zenitsuuchiha76 <zenitsuuchiha76@gmail.com>
2025-10-29 15:28:07 +0600

    Update time.js

--- Script/commands/time.js
+++ Script/commands/time.js
@@ -27,11 +27,11 @@ module.exports.run = async function({ api, event }) {
  const currentTime = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
 
  const imgLinks = [
- "https://i.imgur.com/EuiRi4v.jpeg",
- "https://i.imgur.com/ZjxQx17.jpeg",
- "https://i.imgur.com/dOO6Af5.jpeg",
- "https://i.imgur.com/WMIngcC.jpeg",
- "https://i.imgur.com/2dJSfXq.jpeg"
+ "https://i.imgur.com/zDRxC08.jpeg",
+ "https://i.imgur.com/zDRxC08.jpeg",
+ "https://i.imgur.com/BgLpAFT.jpeg",
+ "https://i.imgur.com/tD4c0Wy.jpeg",
+ "https://i.imgur.com/tD4c0Wy.jpeg"
  ];
 
  const imgPath = __dirname + "/cache/time.jpg";
@@ -45,7 +45,7 @@ module.exports.run = async function({ api, event }) {
 
 ⏱️ 𝗕𝗼𝘁 𝗨𝗽𝘁𝗶𝗺𝗲: ${hours} hour(s), ${minutes} minute(s), ${seconds} second(s)
 
-💠𝗕𝗢𝗧 𝗔𝗗𝗠𝗜𝗡 𝗦𝗔𝗛𝗔𝗗𝗔𝗧~𝗦𝗔𝗛𝗨💠
+💠𝗕𝗢𝗧 𝗔𝗗𝗠𝗜𝗡 𝗚𝗼𝗷𝗼 𝗢𝗿𝘂𝗽𝗲~𝗣𝗶𝘄 𝗣𝗶𝘄💠
 
 
 ¶────██████────¶
@@ -63,7 +63,7 @@ module.exports.run = async function({ api, event }) {
 ¶────◥████◤────¶
 ¶─────◥██◤─────¶
 
-🌟 ─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭 🌟`;
+🌟 ─꯭─⃝‌‌𝐏𝐢𝐰 𝐏𝐢𝐰 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭 🌟`;
 
  const callback = () => {
  api.sendMessage({
