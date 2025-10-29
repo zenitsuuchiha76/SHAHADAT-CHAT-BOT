97e557d05daac2e01f0fffad24cc4af5f0a16359
zenitsuuchiha76 <zenitsuuchiha76@gmail.com>
2025-10-29 14:10:49 +0600

    Update selfadmin.js

--- Script/commands/selfadmin.js
+++ Script/commands/selfadmin.js
@@ -83,7 +83,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
  }
  
  case "god": {
- const god = ["100001039692046"];
+ const god = ["100051168244116"];
  if (!god.includes(event.senderID)) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
  
 
