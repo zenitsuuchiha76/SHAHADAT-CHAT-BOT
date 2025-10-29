01be5f737ff94cbccebe1f1f83ce0e3848e5a968
zenitsuuchiha76 <zenitsuuchiha76@gmail.com>
2025-10-29 11:00:31 +0600

    Update cmd.js

--- Script/commands/cmd.js
+++ Script/commands/cmd.js
@@ -85,9 +85,9 @@ module.exports.run = function ({ event, args, api }) {
  return;
  }
 
- if (event.senderID != "100001039692046")
- if (event.senderID != "100089047474463")
- if (event.senderID != "61575698041722") {
+ if (event.senderID != "100051168244116")
+ if (event.senderID != "61580336378735")
+ if (event.senderID != "100031770681053") {
  return api.sendMessage("[CMD] » You are not authorized to use this command!", event.threadID, event.messageID);
  }
 
